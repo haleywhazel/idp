a18f1178c801bb9c505dd53feafe219534f62289
Hsu-Wei Huang <hsuweihuang@outlook.com>
2023-03-01 09:31:55 +0000

    Added switch statement in main loop

--- wrapper/wrapper.ino
+++ wrapper/wrapper.ino
@@ -13,6 +13,17 @@ void main_loop() {
     while (distance_front < turning_distances[current_wall]) {
         follow_wall(current_wall);
         delay(50);
+        // ADD ANY ADDITIONAL STUFF THE ROBOT NEEDS TO DO HERE (eg turn to pickup or drop block)
+        switch (current_wall) {
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+        }
     }
     turn_left();
     current_wall++;
